d577bee882b22913077f9b95b6ecb9ba91446ca9
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-05-17 12:26:55 +0200

    implemented a clock

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ Screen height: {SCREEN_HEIGHT}""")
     print(f"Initalized with {numpass} passes and {numfail} fails")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+
     # game loop
     while True:
         for event in pygame.event.get():
@@ -23,6 +26,7 @@ Screen height: {SCREEN_HEIGHT}""")
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
