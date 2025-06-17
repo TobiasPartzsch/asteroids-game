80d7e450cd22d8bcbd83d1edae2a4cf15b63c6f0
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-06-17 11:04:28 +0200

    split constants into multiple files

---
+++ settings/display.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class GameColors(Enum):
+    BACKGROUND = "black"
+    FOREGROUND = "white"
+    # Future colors ready to go:
+    # PLAYER = "blue"
+    # ASTEROID = "gray" 
+    # SHOT = "yellow"
+    # UI_TEXT = "green"
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+FPS = 60
+TIMER_FONT = None
+TIMER_FONT_SIZE = 36
