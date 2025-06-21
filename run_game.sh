1566149c2e3044639d93ccdd06a5f97deac8b370
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-06-21 09:18:50 +0200

    created shell script to setup virtual environment and run main.py

---
+++ run_game.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the main Python script
+python main.py
+
+# The virtual environment is automatically deactivated when the script exits
