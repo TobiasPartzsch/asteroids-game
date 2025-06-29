2ba31d0749dcce532f6ef727076700d0f1443643
TobiasPartzsch <Tobias.Partzsch@gmx.de>
2025-06-29 12:41:51 +0200

    defined more detailed speed settings

--- settings/player.py
+++ settings/player.py
@@ -1,8 +1,9 @@
 from src.boundary_behaviors import BoundaryBehavior
 
-
 RADIUS: float = 20.0
-TURN_SPEED: float = 300
-SPEED: float = 200
+TURN_SPEED = 180.0      # degrees per second
+FORWARD_SPEED = 300.0   # pixels per second
+STRAFE_SPEED = 250.0
+BACKWARD_SPEED = 200.0
 SHOOT_COOLDOWN_SECOND: float = 0.3
 BOUNDARY_BEHAVIOR:BoundaryBehavior = BoundaryBehavior.BOUNCE  # IDE autocomplete or see boundary_handlers.BoundaryBehavior enum for options
